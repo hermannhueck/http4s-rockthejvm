e3fefab3f634aec28bceeab96b3446d8ba795093
Hermann Hueck <hermann.hueck@mailbox.org>
2024-01-16 12:35:08 +0100

    bump versions

--- project/Versions.scala
+++ project/Versions.scala
@@ -7,7 +7,7 @@ object Versions {
   val scala2Version = "2.13.12"
 
   // 3rd party libraries
-  val http4sVersion        = "0.23.24"
+  val http4sVersion        = "0.23.25"
   // val fs2Version           = "3.9.3"
   val circeVersion         = "0.15.0-M1"
   val cirisVersion         = "3.5.0"
@@ -17,14 +17,14 @@ object Versions {
   val logbackVersion       = "1.4.14"
   val reaktormonkVersion   = "1.3.1"
   val swaggerUIVersion     = "4.11.1"
-  val slf4jVersion         = "2.0.9"
+  val slf4jVersion         = "2.0.11"
   val http4sJwtAuthVersion = "1.2.1"
   val jwtScalaVersion      = "9.4.5"
   val otpJavaVersion       = "2.0.3"
   val zxingVersion         = "3.5.2"
   val sendGridVersion      = "4.10.1"
   val pureConfigVersion    = "0.17.4"
-  val jcdpVersion          = "2.0.3.1"
+  val jcdpVersion          = "4.0.2"
 
   // compiler plugins
   val kindProjectorVersion    = "0.13.2"
